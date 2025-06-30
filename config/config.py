bc0f59652e486be65e18e03ff9defc994baa79d5
Toos <151813251+xymn2023@users.noreply.github.com>
2025-06-30 21:34:33 +0800

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,40 @@
+from urllib.parse import urlparse
+
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+
+class BotConfig:
+    def __init__(self):
+        self.bot_token = getenv("BOT_TOKEN")
+        self.api_id = getenv("API_ID")
+        self.api_hash = getenv("API_HASH")
+        self.bot_proxy: None | BotConfig._Proxy = self._Proxy(getenv("BOT_PROXY", None))
+        self.parser_proxy: None | str = getenv("PARSER_PROXY", None)
+        self.downloader_proxy: None | str = getenv("DOWNLOADER_PROXY", None)
+
+        self.cache_time = int(ct) if (ct := getenv("CACHE_TIME")) else 600
+        self.ai_summary = bool(getenv("AI_SUMMARY").lower() == "true")
+        self.douyin_api = getenv("DOUYIN_API", None)
+
+    class _Proxy:
+        def __init__(self, url: str):
+            self._url = urlparse(url) if url else None
+            self.url = self._url.geturl() if self._url else None
+
+        @property
+        def dict_format(self):
+            if not self._url:
+                return None
+            return {
+                "scheme": self._url.scheme,
+                "hostname": self._url.hostname,
+                "port": self._url.port,
+                "username": self._url.username,
+                "password": self._url.password,
+            }
+
+
+bot_cfg = BotConfig()
